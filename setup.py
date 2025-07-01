dd79d4dd93fd566c3811b5d5f9672737ec49db30
RoseTheVagabond <rosethevagabond@gmail.com>
2025-07-01 07:54:32 +0200

    Changed file name from main to cli

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ from setuptools import setup
 setup (name = 'ugit', version = '1.0', packages = ['ugit'],
         entry_points = {
             'console_scripts' : [
-                'ugit = ugit.main:main'
+                'ugit = ugit.cli:main'
             ]
         })
